5c560ffc4c1dcbf19318cb52fe3b9f35b3bfa959
Toad Farm <toadfarm@protonmail.com>
2021-04-01 04:56:53 +0200

    Initial Commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,99 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://snek.farm/',
+  },
+  {
+    label: 'Farmhub',
+    icon: 'FarmhubIcon',
+    // href: 'https://farmhub.community',
+    items: [
+      {
+        label: 'Farmhub',
+        href: 'https://farmhub.community',
+        icon: 'FarmhubIcon'
+      },
+      {
+        label: 'Toad.Farm',
+        href: 'https://toad.farm',
+        icon: 'ToadFarmIcon'
+      },
+      {
+        label: 'Mouse.Farm',
+        href: 'https://mouse.farm',
+        icon: 'MouseFarmIcon'
+      },
+      {
+        label: 'Snek.Farm',
+        href: 'https://snek.farm',
+        icon: 'SnekFarmIcon'
+      },
+      {
+        label: 'Eagle.Farm',
+        href: 'https://eagle.farm',
+        icon: 'EagleFarmIcon'
+      },
+      {
+        label: 'Eagle\'s Nest',
+        href: 'https://eaglenest.finance',
+        icon: 'EagleNestIcon'
+      },
+    ]
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://exchange.snek.farm/#/swap',
+        icon: 'TradeIcon',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.snek.farm/#/pool',
+        icon: 'LiquidityIcon',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://snek.farm/farms',
+  },
+  {
+    label: 'Breeding Ground',
+    icon: 'SnekBreeding',
+    href: 'https://snek.farm/nests',
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/farmhubbsc",
+        icon: 'GithubIcon', 
+      },
+      {
+        label: "Docs",
+        href: "https://docs.farmhub.community/",
+        icon: 'BooksIcon',
+      },
+      // {
+      //   label: "Blog",
+      //   href: "https://farmhub.medium.com/",
+      // },
+    ],
+  },
+  {
+    label: 'Audit by Solidity',
+    icon: 'AuditIcon',
+    href: 'https://solidity.finance/audits/Farmhub/',
+  },
+]
+
+export default config
---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x6e74C976E67Feae8E83635936Ef79F969e14E869',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0xf5e5FCBCB37af39DE82054DEa1BDA9D00555550a'
+}
+
+export default priceContracts
